72aa0e0649f6865413bde868c4ff54f046ee786c
md-hasan-ali <hasanaliringku@gmail.com>
2024-01-11 23:46:44 +0600

    Adding new two visual Test

--- cypress/e2e/visualTest/visualTest.cy.js
+++ cypress/e2e/visualTest/visualTest.cy.js
@@ -1,11 +1,25 @@
 /// <reference types="cypress" />
 
 describe('Visuals', () => {
-    it('should compare screenshot of the entire page', () => {
-        cy.visit('https://www.google.com/chrome/')
+    it('Google home page compaire', () => {
+        cy.visit('https://www.google.com/')
         cy.compareSnapshot({
-            name: 'home',
-            testThreshold: 0.9
+            name: 'google-home',
+            testThreshold: 0.5
+        })
+    })
+    it('Facebook loging page compaire', () => {
+        cy.visit('https://www.facebook.com/')
+        cy.compareSnapshot({
+            name: 'fb-home',
+            testThreshold: 0.5
+        })
+    })
+    it('Demoblaze Home page compaire', () => {
+        cy.visit('https://www.demoblaze.com/')
+        cy.compareSnapshot({
+            name: 'demo-home',
+            testThreshold: 0.5
         })
     })
 })
